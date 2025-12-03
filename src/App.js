065944395a65bea7a4caedad9f09273b45a3df01
naruto-uzamaki <kalavalarevanth@gmail.com>
2025-12-03 12:32:43 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [parity, setParity] = useState("");
+  const [data, setData] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const num = Number(data);
+
+    if (isNaN(num)) {
+      setParity("NA");
+      return;
+    }
+
+    if (num % 2 === 0) {
+      setParity("Even");
+    } else {
+      setParity("Odd");
+    };
+  }
+
+  const renderContent = () => {
+    if (parity === "NA") {
+      return <p>Not a valid number!</p>;
+    }
+
+    if (parity === "Even") {
+      return <p>The number {data} is Even!</p>
+    }
+
+    if (parity === "Odd") {
+      return <p>Oops, {data} is odd!</p>
+    }
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+      <h1>Even or Odd Checker</h1>
+      <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <input name='number' placeholder='Enter a number' value={data} onChange={(e) => setData(e.target.value)} />
+        <button type='submit' style={{ padding: "10px", backgroundColor: "blue", color: "white", margin: "10px", borderRadius: "8px" }}>Check</button>
+      </form>
+      {renderContent()};
     </div>
   );
 }
